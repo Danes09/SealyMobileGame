f5248f3e8459f9a3713a0d831d650389e346e6cc
Rajan <themailboxofabi@yahoo.com>
2022-03-12 02:04:28 +0530

    Added Leaderboard using Lootlocker in a separate scene. Can be viewed on the lootlocker website. Done on a separate scene. Member ID (or PlayerID) and score are manually entered... will change this in the future to get the current score from other scripts.

---
+++ Assets/Script/LeaderBoardController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine.UI;
+using LootLocker.Requests;
+using UnityEngine;
+
+public class LeaderBoardController : MonoBehaviour
+{
+    public InputField memberID, playerScore;
+    public int ID; //this refers to the lootlocker leaderboard ID
+
+
+    /*CURRENTLY SET TO MANUALLY ENTERING THE SCORE. SHALL BE MODIFIED TO GET THE SCORE FROM OTHER SCRIPTS*/
+
+    public void Start()
+
+    {
+
+        LootLockerSDKManager.StartSession("Player",(response)=>
+        {
+            if (response.success)
+            {
+                Debug.Log("Success in start");
+            }
+
+            else
+            {
+                Debug.Log("Fail in start");
+            }
+
+
+        }
+
+        );
+    } //start
+
+
+    public void SubmitScore() {
+
+        LootLockerSDKManager.SubmitScore(memberID.text, int.Parse(playerScore.text), ID, (response)=>
+
+        {
+            if (response.success)
+            {
+                Debug.Log("Success in submit score");
+            }
+
+
+            else
+            {
+                Debug.Log("Failure in submit score");
+            }
+
+        }
+
+        );
+    
+    }
+
+
+
+} //class
---
+++ Assets/Script/TagManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager : MonoBehaviour
+{
+    public static string WATER_TAG = "Water";
+    public static string DESTRUCTIBLE_TAG="Destructible";
+    public static string WALL_TAG = "Wall";
+    public static string BALL_TAG = "Ball";
+    public static string ITEM_TAG = "Item";
+    public static string ENEMY_TAG = "Enemy";
+
+
+    public static string FXTEMPORAIRE_TAG = "FxTemporaire";
+    public static string OBJECT_TAG = "Object";
+    public static string ALLY_TAG = "Ally";
+    public static string BALLOON_TAG = "Balloon";
+    
+    
+    public static string PLAYER_TAG = "Player";
+
+}
