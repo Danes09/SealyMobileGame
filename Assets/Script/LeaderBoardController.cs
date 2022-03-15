2258e72993a153837e1c9e726f2f20af43bc0240
Rajan <themailboxofabi@yahoo.com>
2022-03-16 03:26:24 +0530

    Leaderboard appears on Scene
    Leaderboard appears on scene with an index and score but not ID.

--- Assets/Script/LeaderBoardController.cs
+++ Assets/Script/LeaderBoardController.cs
@@ -7,6 +7,10 @@ public class LeaderBoardController : MonoBehaviour
     public InputField memberID, playerScore;
     public int ID; //this refers to the lootlocker leaderboard ID
 
+    public int MaxScores = 10; //number of scores that can be displayed
+
+    public Text[] Entries;
+
 
     /*CURRENTLY SET TO MANUALLY ENTERING THE SCORE. SHALL BE MODIFIED TO GET THE SCORE FROM OTHER SCRIPTS*/
 
@@ -32,6 +36,57 @@ public class LeaderBoardController : MonoBehaviour
         );
     } //start
 
+    public void ShowScores()
+    {
+        LootLockerSDKManager.GetScoreList(ID, MaxScores, (response) => {
+
+            if (response.success)
+            {
+                //LootLockerMember has become LootLockerLeaderboardMember
+
+                LootLockerLeaderboardMember[] scores = response.items;
+                //LootLockerMember[] scores = response.items;
+
+                for (int i = 0; i < scores.Length; i++)
+                {
+                    Entries[i].text = (scores[i].rank + ".   " + scores[i].score);
+
+                }
+
+                if (scores.Length < MaxScores)
+                {
+
+                    for (int i = scores.Length; i < MaxScores; i++)
+                    {
+                        //Entries[i].text = (i + 1).ToString() + ".   none"; //no reason to convert to string
+                        Entries[i].text = (i + 1) + "." + "  none";
+                    }
+
+                    if (scores.Length == MaxScores)
+                    {
+
+                        for (int i = scores.Length; i < MaxScores; i++)
+                        {
+                            //Entries[i].text = (i + 1).ToString() + ".   none"; //no reason to convert to string
+                            Entries[i].text = (i + 1) + "."+ "none"; //fewer spaces to accommodate #10
+                        }
+
+
+
+                    }
+
+                }
+
+                else
+                {
+                    Debug.Log("Fail in show scores");
+                }
+
+
+            }
+        }
+        );
+    } //show scores
 
     public void SubmitScore() {
 
