a93eea68ac1e4259454eb16d6638fb2e1898768b
YapKarSeng <0121537@kdu-online.com>
2022-06-02 17:35:11 +0800

    Fix 5 (Karseng)

--- Assets/Script/NextLevel.cs
+++ Assets/Script/NextLevel.cs
@@ -9,7 +9,7 @@ public class NextLevel : MonoBehaviour
     {
         if (oth.CompareTag("Player"))
         {
-           // Debug.Log("touch");
+            // Debug.Log("touch");
             SceneManager.LoadScene(4);
 
         }
