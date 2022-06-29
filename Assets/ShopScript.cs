394266603c111c2f23f63a779d36647b08c6f979
YapKarSeng <0121537@kdu-online.com>
2022-06-29 20:52:25 +0800

    ShopUI

---
+++ Assets/ShopScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopScript : MonoBehaviour
+{
+    public Slider healthSlider;
+
+    public int maxHealth;
+
+    int currentHealth;
+
+    void Start()
+    {
+        SetDefs();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayerPrefs.SetInt("health", 0);
+            Debug.Log(PlayerPrefs.GetInt("health", 0));
+        }
+    }
+
+    void SetDefs()
+    {
+        currentHealth = PlayerPrefs.GetInt("health",0);
+
+        healthSlider.maxValue = maxHealth;
+
+        healthSlider.value = currentHealth;
+        Debug.Log(PlayerPrefs.GetInt("health", 0));
+
+    }
+
+    public void buyHealth()
+    {
+        if(currentHealth < maxHealth)
+        {
+            currentHealth += 5;
+            PlayerPrefs.SetInt("health", currentHealth);
+            healthSlider.value = currentHealth;
+            Debug.Log("Health Upgrade");
+        }
+        else
+        {
+            Debug.Log("Health full");
+        }
+    }
+}
