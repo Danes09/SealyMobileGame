ffb2cf9fa60f378562caa67ed9b178570f7810ef
BrotherMus <Musabanas99@gmail.com>
2022-10-18 21:00:48 +0800

    level 1(with player)

--- Assets/Script/Scripts/PlayerManagerScript.cs
+++ Assets/Script/Scripts/PlayerManagerScript.cs
@@ -261,7 +261,7 @@ public class PlayerManagerScript : MonoBehaviour
             {
                 TCS.DecreaseHunger();
             }
-            else if (TCS.currHungerValue <= 0)
+            else if (Scenetuf == true && TCS.currHungerValue <= 0)
             {
                 Scenetuf = false;
             }
